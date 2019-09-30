db3646462c3fb760e0fef194d595a9323bf1387b
Fabian Marcus <apps@fabian-marcus.de>
2019-09-30 16:11:58 +0200

    Refactoring - "phx"-Präfix entfernt + header & footer styles hinzugefügt, statt zu referenzieren

--- src/index.js
+++ src/index.js
@@ -4,8 +4,3 @@
 require('./index.scss');
 require('./fonts/titillium.scss');
 require('./fonts/fontawesome.scss');
-
-// Phoenix Reisen Komponenten
-require('../node_modules/@phoenixreisen/header/src/phx.header.style.scss');
-require('../node_modules/@phoenixreisen/footer/src/footer.style.scss');
-require('../node_modules/@phoenixreisen/tabs/src/phx.tabs.style.scss');
